648669bff28f657e057a00050177cfc2f5800f57
Michael W. Jordan <kaleb700@yahoo.com>
2014-02-03 20:45:19 -0500

    lessson 30 homework problems
    8 answers to practice problems

---
+++ hashHW.rb
@@ -0,0 +1,33 @@
+=begin
+1. hash = {"x" => 3, "y" => 2, "z" => 1}
+
+2. hash["y"]
+
+3. array[0]
+
+4. ice_cream.push("strawberry")
+
+5. atlanta_teams["basketball"] = "hawks"
+
+6. numbers[2][1]
+
+7. atlanta_teams["hawks_players"]["power forward"]
+
+8. 
+a. hash
+b. array
+c. hash
+d. hash
+
+=end
+
+
+
+atlanta_teams = {"baseball" => "braves", "football" => "falcons", 
+"hawks_players" => {"pointguard" => "Jeff Teague" , "power forward" => "Paul Milsap", 
+"forward/center" => "Al Horford"}}
+
+puts atlanta_teams
+
+atlanta_teams["hawks_players"]["power forward"]
+
